799b1bdef147a3a9884c121ab986923ffa24042d
wataru.endo <endo0430w@gmail.com>
2024-03-10 04:37:43 +0900

    Add error process

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // 指定された人物の挨拶を返す
-func Hello(name string) string {
-	// nameを埋め込んだ挨拶を返す
+func Hello(name string) (string, error) {
+	// 名前が渡されなければエラーと一緒にメッセージを返す
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// nameを受け取った場合は、nameをmessageに埋め込む
 	message := fmt.Sprintf("Hi, %v. welcome", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,23 @@ package main
 import (
 	"example.com/greetings"
 	"fmt"
+	"log"
 )
 
 func main() {
+	// 以下を含む、定義済みロガーのプロパティを設定する
+	// ログ・エントリーのprefixと出力を無効にするフラグ
+	// 時間、ソースファイル、行番号
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// greetingsメッセージをもらい、それを出力
-	message := greetings.Hello("Glabys")
+	message, error := greetings.Hello("")
+
+	// エラーが返された場合は、それをコンソールに表示しプログラムを終了する。
+	if error != nil {
+		log.Fatal(error)
+	}
+
 	fmt.Println(message)
 }
